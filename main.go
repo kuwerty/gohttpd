4eb39353cbed6f5cd2f6769477e604d375b21d6f
Krister Wombell <kuwerty@gmail.com>
2016-01-19 12:18:44 +0800

    Add support for serving over TLS.
    Generate keys with genkeys.sh first then add the -secure option.

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
   "flag"
 )
 
+var cache = flag.Bool("cache", true, "enable cached response headers")
+var debug = flag.Bool("debug", false, "enable debugging")
+var secure = flag.Bool("secure", false, "server over TLS (requires server.key, server.pem)")
+
 type DebugWriter struct {
   http.ResponseWriter
   Code int
@@ -24,12 +28,21 @@ func mainHandler(root string) http.Handler {
 
     dw := DebugWriter{ResponseWriter:w}
 
+    w.Header().Set("Cache-Control", "no-cache")
+
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+
     files.ServeHTTP(&dw, r);
 
     h := dw.Header()
 
-    log.Printf(" %d %s %s %s %s\n", dw.Code, r.Method, r.RequestURI, h.Get("Content-Type"), h.Get("Content-Length"))
+    log.Printf("%d %s %s %s %s\n", dw.Code, r.Method, r.RequestURI, h.Get("Content-Type"), h.Get("Content-Length"))
 
+    if *debug {
+      for k,v := range(r.Header) {
+        log.Printf("  %s: %s\n", k, v)
+      }
+    }
   }
 
   return http.HandlerFunc(foo)
@@ -42,6 +55,11 @@ func main() {
   flag.Parse()
 
   log.Printf("Ready on %s with root %s\n", *addr, *root)
+
+  if(*secure) {
+    log.Printf("Serving TLS")
+    log.Fatal(http.ListenAndServeTLS(*addr, "server.pem", "server.key", mainHandler(*root)))
+  }
 
   log.Fatal(http.ListenAndServe(*addr, mainHandler(*root)))
 }
