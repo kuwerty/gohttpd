e352cfed72be82cf457afa1d0d94eb50af8719f5
Krister Wombell <kuwerty@gmail.com>
2017-02-03 15:04:33 +0800

    Added a simple config file to specify remapped paths.
    Example:
    cert-file=server.pem
    key-file=server.key
    
    #remap paths in format server-path=local-Path
    #stupid example
    [paths]
    /css=out/css
    /js=out/js
    /=public

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
   "log"
   "net/http"
   "flag"
+  "mime"
+  "github.com/go-ini/ini"
 )
 
-var cache = flag.Bool("cache", true, "enable cached response headers")
-var debug = flag.Bool("debug", false, "enable debugging")
+var cache  = flag.Bool("cache", true, "enable cached response headers")
+var debug  = flag.Bool("debug", false, "enable debugging")
 var secure = flag.Bool("secure", false, "server over TLS (requires server.key, server.pem)")
+var addr   = flag.String("addr", ":8000", "listen address")
+var root   = flag.String("root", ".", "www root directory")
 
 type DebugWriter struct {
   http.ResponseWriter
@@ -21,22 +25,26 @@ func (w *DebugWriter) WriteHeader(code int) {
 }
 
 
-func mainHandler(root string) http.Handler {
-  files := http.FileServer(http.Dir(root))
+func wrapHandler(name string, handler http.Handler) http.Handler {
 
   foo := func(w http.ResponseWriter, r *http.Request) {
 
     dw := DebugWriter{ResponseWriter:w}
 
-    w.Header().Set("Cache-Control", "no-cache")
+    if(!*cache) {
+      w.Header().Set("Cache-Control", "no-cache")
+    } else {
+      w.Header().Set("Cache-Control", "max-age=120")
+    }
 
     w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "*") //POST, PUT, GET, OPTIONS, HEAD")
 
-    files.ServeHTTP(&dw, r);
+    handler.ServeHTTP(&dw, r);
 
     h := dw.Header()
 
-    log.Printf("%d %s %s %s %s\n", dw.Code, r.Method, r.RequestURI, h.Get("Content-Type"), h.Get("Content-Length"))
+    log.Printf("%d %s %s (%s) %s %s\n", dw.Code, r.Method, r.RequestURI, name, h.Get("Content-Type"), h.Get("Content-Length"))
 
     if *debug {
       for k,v := range(r.Header) {
@@ -49,18 +57,63 @@ func mainHandler(root string) http.Handler {
 }
 
 
+
 func main() {
-  addr := flag.String("addr", ":8000", "listen address")
-  root := flag.String("root", ".", "www root directory")
   flag.Parse()
+
+  certFile := "server.pem"
+  keyFile := "server.key"
+
+  mux := http.NewServeMux()
+
+  var cfg, err = ini.LoadSources(ini.LoadOptions{AllowBooleanKeys: true}, "gohttpd.conf")
+
+  if cfg != nil && err == nil {
+    log.Printf("found a config file\n")
+
+    var defaults = cfg.Section("")
+
+    if defaults.HasKey("cert-file") {
+      certFile = defaults.Key("cert-file").String()
+    }
+
+    if defaults.HasKey("key-file") {
+      keyFile = defaults.Key("key-file").String()
+    }
+
+    var paths = cfg.Section("paths")
+
+    for _,key := range(paths.KeyStrings()) {
+
+      val := paths.Key(key).String()
+
+      log.Printf("mapping %s to %s", key, val)
+
+      handler := http.FileServer(http.Dir(val))
+      handler = http.StripPrefix(key, handler)
+      handler = wrapHandler(val, handler)
+
+      mux.Handle(key, handler)
+    }
+
+  } else {
+
+    mux.Handle("/", wrapHandler("/", http.FileServer(http.Dir(*root))))
+
+  }
+
+
+  mime.AddExtensionType(".aex", "application/json")
 
   log.Printf("Ready on %s with root %s\n", *addr, *root)
 
   if(*secure) {
+    log.Printf("using cert-file %s\n", certFile)
+    log.Printf("using key-file %s\n", keyFile)
     log.Printf("Serving TLS")
-    log.Fatal(http.ListenAndServeTLS(*addr, "server.pem", "server.key", mainHandler(*root)))
+    log.Fatal(http.ListenAndServeTLS(*addr, certFile, keyFile, mux))
   }
 
-  log.Fatal(http.ListenAndServe(*addr, mainHandler(*root)))
+  log.Fatal(http.ListenAndServe(*addr, mux))
 }
 
